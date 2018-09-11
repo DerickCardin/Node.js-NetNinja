c27f2338d98a10ca43dc0dcca6049ed2feeb34ec
Derick Cardin <derickc50@gmail.com>
2018-09-11 21:45:26 +0000

    Lesson 7

--- script.js
+++ script.js
@@ -32,3 +32,8 @@ $(document).ready(function(){
   });
 
 });
+
+var stuff = require('./stuff');
+console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
+console.log(stuff.adder(5,6));
+console.log(stuff.adder(stuff.pi, 5));
