be1bfd0beafe13e4f5aa547080ddc9cdf588f426
Derick Cardin <derickc50@gmail.com>
2018-09-11 22:41:02 +0000

    Lesson 11

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 
-
-fs.readFile('README.MD', 'utf8', function(err, data){
-    fs.wrtieFile('writeMe.txt', data);
+fs.unlink('./stuff/writeMe.txt', function(){
+ fs.rmdir('stuff');   
 });
