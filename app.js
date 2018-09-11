512788c5a0fc62f091dc16ef110f2529a81026a3
Derick Cardin <derickc50@gmail.com>
2018-09-11 22:32:17 +0000

    Lesson 9

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
-var events = require('events');
+var fs = require('fs');
 
-var util = require('util');
 
-var Person = function(name){
-    this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var james = new Person('james');
-var mary = new Person('mary');
-var ryu = new Person('ryu');
-var people = [james, mary, ryu];
-
-people.forEach(function(person){
-    person.on('speak', function(mssg){
-        console.log(person.name + ' said: ' + mssg);
-    });
+fs.readFile('README.MD', 'utf8', function(err, data){
+    fs.wrtieFile('writeMe.txt', data);
 });
-
-james.emit('speak', 'hey dudes');
-ryu.emit('speak', 'I want curry');
