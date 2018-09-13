cbc551a1e43509203cecc4744edda7a12ae4defd
Derick Cardin <derickc50@gmail.com>
2018-09-13 22:01:22 +0000

    Lesson 17

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ myReadStream.pipe(myWriteStream);
 
 var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var myReadStream = fs.createReadStream(__dirname + '/README.MD', 'utf8');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
 myReadStream.pipe(res);
 });
 
 
-server.listen(process.env.PORT, 'process.env.IP');
+server.listen(process.env.PORT, process.env.IP);
 console.log('yo dawgs, now listening to port 3000');
