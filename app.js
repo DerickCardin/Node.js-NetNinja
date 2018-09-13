32a7c533997be31f52cf8af06e377292e81c4421
Derick Cardin <derickc50@gmail.com>
2018-09-13 22:49:19 +0000

    Lesson

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var server = http.createServer(function(req, res){
     if(req.url === '/home' || '/'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         fs.createReadStream(__dirname + '/index.html').pipe(res);
-    } else if (req.url === '/contact'){
+    } else if (req.url === '/contact-us'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         fs.createReadStream(__dirname + '/Contact.html').pipe(res);
     } else if (req.url === '/api/ninjas'){
