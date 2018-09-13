f6cbe22fd1219aff1ae246446d7358e148c4ba43
Derick Cardin <derickc50@gmail.com>
2018-09-13 21:35:18 +0000

    Lesson 15

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var http = require('http');
 
-var server = http.createServer(function(req, res){
-    console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey ninjas');
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname + '/README.MD', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+
+
+myReadStream.on('data', function(chunk){
+   console.log('new chunk received:');
+   myWriteStream.write(chunk);
 });
 
 
-server.listen(3000, '127.0.0.1');
-console.log('yo dawgs, now listening to port 3000');
+
+
+
+
+
+
+
+//var server = http.createServer(function(req, res){
+//    console.log('request was made: ' + req.url);
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    res.end('Hey ninjas');
+//});
+
+
+//server.listen(3000, '127.0.0.1');
+//console.log('yo dawgs, now listening to port 3000');
