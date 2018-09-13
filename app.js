0a1e1da4925107c4b01505c307988040e237ff3e
Derick Cardin <derickc50@gmail.com>
2018-09-13 22:22:52 +0000

    Lesson 19

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var myObj = {
-        name:'Ryu',
-        job: 'Ninja',
-        age:29
-    };
-    res.end(JSON.stringify(myObj));
+    if(req.url === '/home' || '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if (req.url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/Contact.html').pipe(res);
+    } else if (req.url === '/api/ninjas'){
+        var ninjas = [{name: 'ryu', age:29}, {name: 'yoshi', age: 32}];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(ninjas));
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
     
     
 });
