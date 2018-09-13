25edf08ae467f9890920d3b0c6e4a5d3e6b7747a
Derick Cardin <derickc50@gmail.com>
2018-09-13 21:52:30 +0000

    lesson 16

--- app.js
+++ app.js
@@ -6,25 +6,18 @@ var myReadStream = fs.createReadStream(__dirname + '/README.MD', 'utf8');
 var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
 
-myReadStream.on('data', function(chunk){
-   console.log('new chunk received:');
-   myWriteStream.write(chunk);
+myReadStream.pipe(myWriteStream);
+
+
+
+
+var server = http.createServer(function(req, res){
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var myReadStream = fs.createReadStream(__dirname + '/README.MD', 'utf8');
+myReadStream.pipe(res);
 });
 
 
-
-
-
-
-
-
-
-//var server = http.createServer(function(req, res){
-//    console.log('request was made: ' + req.url);
-//    res.writeHead(200, {'Content-Type': 'text/plain'});
-//    res.end('Hey ninjas');
-//});
-
-
-//server.listen(3000, '127.0.0.1');
-//console.log('yo dawgs, now listening to port 3000');
+server.listen(process.env.PORT, 'process.env.IP');
+console.log('yo dawgs, now listening to port 3000');
