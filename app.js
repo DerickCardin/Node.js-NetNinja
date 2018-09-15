363c85a911c93abc0af070ec5deff947465364f7
Derick Cardin <derickc50@gmail.com>
2018-09-15 14:58:51 +0000

    Lesson 28

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
    res.render('index'); 
 });
 
 app.get('/contact', function(req, res){
-   res.render('contact'');  
+   res.render('contact');  
 });
 
 app.get('/profile/:name', function(req, res){
